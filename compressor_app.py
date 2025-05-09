0a5809068ba2cdb185d18b64a6ad09573678fb95
yukmr-test <goglsis105@gmail.com>
2025-05-09 20:33:50 +0900

    送受信ポート調整

--- compressor_app.py
+++ compressor_app.py
@@ -37,5 +37,5 @@ def webhook():
     return "OK", 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))  # Renderが自動で指定するポートを取得
     app.run(host="0.0.0.0", port=port)
